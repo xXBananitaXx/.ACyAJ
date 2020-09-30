84fd8b0031106ff2e7c51f1c02ec80306e5d5698
xXBananitaXx <72148563+xXBananitaXx@users.noreply.github.com>
2020-09-30 11:22:41 -0500

    Add files via upload

---
+++ css/js/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+
+	$("#banner").css({"height":$(window).height() + "px"});
+
+	var flag = false;
+	var scroll;
+
+	$(window).scroll(function(){
+		scroll = $(window).scrollTop();
+
+		if(scroll > 200){
+			if(!flag){
+				$("#logo").css({"margin-top": "-5px", "width": "90px","height":"90px"});
+
+				$("header").css({});
+				flag = true;
+			}
+		}else{
+			if(flag){
+				$("#logo").css({"margin-top": "150px", "width": "250px","height":"250px"});
+
+				$("header").css({"background-color": "transparent"});
+				flag = false;
+			}
+		}
+
+
+	});
+
+});
